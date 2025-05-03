8f278a67b895f7fd1c930b63776dd49f06f5619d
Raviyadav4224 <Raviyadav4224@gmail.com>
2025-05-03 13:27:22 +0530

    deepFlatten, Polyfill for getElementById & getElementsByClassName, Rate Limiting API, promiseOneByOne, Memoize Function, Polyfill for filter method

--- TrickyQuestionsJS.js
+++ TrickyQuestionsJS.js
@@ -76,6 +76,7 @@ function deepCopyWithCircularReferences(value, visited) {
 }
 
 // Implement the functionality behaviour of Promise.any
+
 async function promiseAll(promiseArray) {
   console.log("promiseArray", promiseArray);
   if (!Array.isArray(promiseArray)) {
@@ -97,18 +98,25 @@ async function promiseAll(promiseArray) {
 }
 
 // Implement a function that recursively flattens an array into a single level deep.
-function flattenArray(value) {
-  return value.flat(1);
-}
-function deepFlattenArray(value) {
-  let result = [];
-  value.forEach((element) => {
+
+function deepFlatten(arr) {
+  const result = [];
+
+  function flat(element) {
     if (Array.isArray(element)) {
-      flattenArray(element).forEach((item) => result.push(item));
+      for (const item of element) {
+        flat(item);
+      }
     } else {
       result.push(element);
     }
-  });
+  }
+
+
+  for (const element of arr) {
+    flat(element);
+  }
+
   return result;
 }
 
@@ -116,22 +124,45 @@ function deepFlattenArray(value) {
 
 function getElementById(id) {
   function traverse(node) {
+    // Checks if ID is present in the node or not
     if (node.id === id) {
       return node;
     }
 
-    for (let child of node.children) {
-      let result = traverse(child);
-      if (result) {
-        return result;
-      }
+    for (const child of node.children) {
+      // Check for all childs
+      const result = traverse(child);
+      if (result) return result;
+    }
+
+    // if no match found
+    return null;
+  }
+
+  return traverse(document.body);
+}
+
+//Implement getElementsByClassName
+
+function getElementsByClassName(classname) {
+  const result = [];
+  function traverse(node) {
+    // Check if node contains the given class name then add it to result
+    if (node.classList.contains(classname)) {
+      result.push(node);
+    }
+
+    // Check for all Childs whether they contain the given classname or not
+
+    for (const child of node.children) {
+      traverse(child);
     }
   }
 
   traverse(document.body);
-  return null;
-}
 
+  return result;
+}
 function deepClone(obj) {
   if (obj === null || typeof obj !== "object") {
     return obj;
@@ -143,3 +174,66 @@ function deepClone(obj) {
   }
   return clone;
 }
+
+const rateLimitApi = (timeSpan, rate, callback) => {
+  let count = 0;
+  let lastTime = 0;
+  return function (...args) {
+    let currentTime = Date.now();
+    console.log(currentTime - lastTime, timeSpan, count);
+    if (currentTime - lastTime >= timeSpan) {
+      lastTime = currentTime;
+      count = 0;
+    }
+    if (count <= rate) {
+      count += 1;
+      callback(...args);
+    }
+  };
+};
+
+// one promise finishes and then next one starts
+
+async function handlePromisesOneByOne() {
+  let res = [];
+  for (const promise of [1, 2, 3, 4, 5, 6]) {
+    try {
+      const data = await getProducts(promise);
+      res.push(data);
+    } catch (error) {
+      console.log(error.message);
+    }
+  }
+  return res;
+}
+handlePromisesOneByOne().then((res) => console.log(res));
+
+function memoizeFunc(func) {
+  let cache = new Map();
+
+  return function (...args) {
+    const key = JSON.stringify(args);
+    if (cache.has(key)) {
+      console.log("From Cache");
+      return cache.get(key);
+    } else {
+      const result = func.call(this, ...args);
+      cache.set(key, result);
+      return result;
+    }
+  };
+}
+
+// Polyfill for Filter method
+
+Array.prototype.mapFilter = function (callbackFn) {
+  let result = [];
+
+  this.map((item, index, arr) => {
+    if (callbackFn(item, index, arr)) {
+      result.push(item);
+    }
+  });
+
+  return result;
+};
